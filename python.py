67483016d2c0b480199f2b48f2bd9cd997a2abb2
kadir-zeren <k4dzeren@gmail.com>
2024-05-06 13:36:22 +0300

    append

--- python.py
+++ python.py
@@ -1,2 +1,40 @@
 listem = ['a','b','c','d']
 print(type(listem))
+print(listem)
+word = 'happy'
+print(list(word))
+print(list('a b c'))
+text = 'Clarusway'
+print(list(text))
+print([text])
+listem = [1,2,3]
+print(len(listem))
+print(list(listem))
+yeni= [listem]
+print(yeni)
+print(len(yeni))
+a=[[1,2,3],['ali','ayse'],[True,False]]
+print(len(a))
+print(list(a))
+liste_a = [a]
+print([a])
+print(len(liste_a))
+print(len(a))
+
+text ="2023's perfect"
+print(list(text))
+print([text])
+
+print(list('1234'))
+listem = [[1,2,3], 'bir string', 7.23,True]
+print(len(listem))
+
+bos1 = []
+print(len(bos1))
+bos2 = list()
+print(len(bos2))
+
+numbers = [1,4,7]
+numbers.append(9)
+numbers.append('9')
+print(numbers)
